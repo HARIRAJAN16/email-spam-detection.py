c5770fdbd9e1104372ee9a38976cfc27af857d8d
HARIRAJAN16 <147170127+HARIRAJAN16@users.noreply.github.com>
2025-05-13 13:04:59 +0530

    Create email spam detection.py

---
+++ email spam detection.py
@@ -0,0 +1,6 @@
+my-email-spam-project/
+│
+├── email-spam-detection.py       # Main Streamlit app file
+├── spam_classifier.pkl           # Trained spam model
+├── tfidf_vectorizer.pkl          # TF-IDF vectorizer
+└── requirements.txt              # Dependencies file
